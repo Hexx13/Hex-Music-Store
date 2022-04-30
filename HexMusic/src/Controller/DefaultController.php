55e25354647d3313a86719357a80c16b9f06c4d7
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-04-30 18:13:51 +0100

    Add route for store catalog

--- HexMusic/src/Controller/DefaultController.php
+++ HexMusic/src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,15 @@ class DefaultController extends AbstractController
     {
         $template = 'default/index.html.twig';
         $argsArray = [];
+
+        return $this->render($template, $argsArray);
+    }
+
+    #[Route('/catalog', name: 'catalog')]
+    public function catalog(ProductRepository $productRepository): Response
+    {
+        $template = 'catalog/catalog.html.twig';
+        $argsArray = ['products'=>$productRepository->findAll()];
 
         return $this->render($template, $argsArray);
     }
