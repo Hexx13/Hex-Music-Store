69f55842080d9335629767941bbbb3d2f37b40b9
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-05-01 19:27:39 +0100

    create lesson form

--- HexMusic/src/Controller/DefaultController.php
+++ HexMusic/src/Controller/DefaultController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Lesson;
+use App\Entity\User;
+use App\Form\LessonType;
+use App\Form\RegistrationFormType;
 use App\Repository\ProductRepository;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class DefaultController extends AbstractController
 {
@@ -26,4 +35,29 @@ class DefaultController extends AbstractController
 
         return $this->render($template, $argsArray);
     }
+    #[Route('/booklesson', name: 'bookLesson')]
+    public function bookLesson(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $lesson = new Lesson();
+        $form = $this->createForm(LessonType::class, $lesson);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+
+
+            $entityManager->persist($lesson);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('default/bookLesson.html.twig', [
+            'bookForm' => $form->createView(),
+        ]);
+    }
+
+
+
 }
