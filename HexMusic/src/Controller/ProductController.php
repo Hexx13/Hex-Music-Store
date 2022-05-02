03a47002fd1438ad0601f5a3c6f8535406952b40
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-05-02 18:20:57 +0100

    Add image upload for listing products

--- HexMusic/src/Controller/ProductController.php
+++ HexMusic/src/Controller/ProductController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 #[Route('/product')]
 class ProductController extends AbstractController
@@ -29,6 +30,20 @@ class ProductController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $uploadedFile = $form['imageFile']->getData();
+            $destination = $this->getParameter('kernel.project_dir').'/public';
+
+            $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+            $newFilename = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+            $uploadedFile->move(
+                $destination,
+                $newFilename
+            );
+
+            $product->setImage($newFilename);
+
             $productRepository->add($product);
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
--- HexMusic/src/Form/Product1Type.php
+++ HexMusic/src/Form/Product1Type.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,10 @@ class Product1Type extends AbstractType
             ->add('name')
             ->add('price')
             ->add('type')
-            ->add('image')
+           // ->add('image')
+            ->add('imageFile', FileType::class,[
+                'mapped' => false
+            ])
         ;
     }
 
