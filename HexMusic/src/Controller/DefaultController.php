2c91e2b12d7ecf5d9b8337510ec3ff70c005ccdb
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-05-03 21:23:16 +0100

    add testing utility

--- HexMusic/src/Controller/DefaultController.php
+++ HexMusic/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,6 +33,7 @@ class DefaultController extends AbstractController
     {
         return $this->render('booking/calendar.html.twig');
     }
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/adminpanel', name: 'admin_panel', methods: ['GET'])]
     public function adminPanel(): Response
     {
