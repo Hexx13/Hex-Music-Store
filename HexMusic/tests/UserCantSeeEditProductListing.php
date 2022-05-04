fe4e548f521ab602c50fcd9de7d0707057237890
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-05-04 12:00:15 +0100

    Add edit visibility test

---
+++ HexMusic/tests/UserCantSeeEditProductListing.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class UserCantSeeEditProductListing extends WebTestCase
+{
+    public function testHomePageTitleText(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('nav', 'Catalog');
+    }
+
+    public function testUserCanAccessCatalog(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        //user login
+        $userName = 'user';
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByusername($userName);
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/catalog');
+
+        $responseCode = $client->getResponse()->getStatusCode();
+        $this->assertSame(Response::HTTP_OK, $responseCode);
+    }
+
+    public function testUserCanAccessCatalogProduct(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        //user login
+        $userName = 'user';
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByusername($userName);
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/product/1');
+
+        $responseCode = $client->getResponse()->getStatusCode();
+        $this->assertSame(Response::HTTP_OK, $responseCode);
+    }
+
+    public function testUserCantSeeEditOnProduct(): void
+    {
+        $client = static::createClient();
+
+        //user login
+        $userName = 'user';
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByusername($userName);
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/product/1');
+
+        $responseCode = $client->getResponse()->getStatusCode();
+        $this->assertSame(Response::HTTP_OK, $responseCode);
+        $this->assertSelectorTextNotContains('body', 'Edit');
+    }
+}
